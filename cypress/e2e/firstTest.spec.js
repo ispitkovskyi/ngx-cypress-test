945563c4e7a1178bd7de40d2fea8ebad6e97f7d0
ispitkovskyi <spitkovskyigor@gmail.com>
2024-08-15 22:31:12 +0300

    working with text

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -52,7 +52,7 @@ describe('first test suite', () => {
             .click()
     })
 
-    it.only('save object of the command', () => {
+    it('save object of the command', () => {
         cy.visit('/') // Enough to specify '/', Cypress will navigate to the baseUrl specified in the cypress.config.js
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -71,6 +71,51 @@ describe('first test suite', () => {
             // Need to convert the 'usingTheGridForm' JQuery object back to Cypress object
             cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
             cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+        })
+    })
+
+    it.only('extract text values', () => {
+        cy.visit('/') // Enough to specify '/', Cypress will navigate to the baseUrl specified in the cypress.config.js
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // 1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        // 2 (JQuery)
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            // label is JQuery object, so we can use all the methods of JQuery objects
+            const labelText = label.text()
+            // expect assertion can be used only inside JQuery syntax block (this callback function)
+            expect(labelText).to.equal('Email address')
+
+            // should assertion requires conversion of html text to Cypress object, by calling wrap() method
+            cy.wrap(labelText).should('contain', 'Email address')
+        })
+
+        // 3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.get('[for="exampleInputEmail1"]').invoke('text').should('contain', 'Email address')
+
+        // Text value saved to an alias, and can be used later on in the code
+        cy.get('[for="exampleInputEmail1"]').invoke('text').as('labelText').should('contain', 'Email address')
+
+        // Check value of attribute
+        cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then( classValue => {
+            expect(classValue).to.equal('label')
+        })
+
+        // Invoke property
+        cy.get('#exampleInputEmail1').type('test@test.com')
+
+        //way 1
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com')
+        //way 2
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').then( propValue => {
+            expect(propValue).to.equal('test@test.com')
         })
     })
 })
